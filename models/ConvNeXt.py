7692502d17dbb9f23a66e21cc936457a4e8a7af8
Dong Sheng <fritingo.ai10@nycu.edu.tw>
2023-06-27 03:22:04 +0800

    ConvNeXt

---
+++ models/ConvNeXt.py
@@ -0,0 +1,123 @@
+# @Article{liu2022convnet,
+#   author  = {Zhuang Liu and Hanzi Mao and Chao-Yuan Wu and Christoph Feichtenhofer and Trevor Darrell and Saining Xie},
+#   title   = {A ConvNet for the 2020s},
+#   journal = {Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)},
+#   year    = {2022},
+# }
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.models.layers import trunc_normal_, DropPath
+from thop import profile
+
+
+class Block(nn.Module):
+    #DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+
+    def __init__(self, dim, drop_path=0.):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.pwconv1 = nn.Conv2d(dim, 4*dim, 1, stride=1, padding=0) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Conv2d(4*dim, dim, 1, stride=1, padding=0)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        norm = nn.LayerNorm(x.size()[1:], eps=1e-6)
+        x = norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        x = input + self.drop_path(x)
+        return x
+    
+class ConvNeXt(nn.Module):
+    
+    def __init__(self, in_chans=3, input_size=224, class_num=100, 
+                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0., head_init_scale=1.,):
+        super().__init__()
+        
+        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
+        self.stem_conv = nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4)
+
+        norm_size_list = []
+        # compute shape by doing a forward pass
+        with torch.no_grad():
+            fake_input = torch.randn(1, in_chans, input_size, input_size)
+            out        = self.stem_conv(fake_input)
+            norm_size  = out.size()[1:]
+            norm_size_list.append(norm_size)
+            for i in range(3):
+                norm_size_list.append([dims[i+1], norm_size[1]//(2*(i+1)), norm_size[2]//(2*(i+1))])
+
+        
+        stem = nn.Sequential(
+            self.stem_conv,
+            nn.LayerNorm(norm_size, eps=1e-6)
+        )
+
+        self.downsample_layers.append(stem)
+
+        for i in range(3):
+            
+            downsample_layer = nn.Sequential(
+                    nn.LayerNorm(norm_size_list[i], eps=1e-6),
+                    nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
+        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+        cur = 0
+        for i in range(4):
+            stage = nn.Sequential(
+                *[Block(dim=dims[i], drop_path=dp_rates[cur + j]) for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        self.head = nn.Linear(dims[-1], class_num)
+
+        self.apply(self._init_weights)
+        self.head.weight.data.mul_(head_init_scale)
+        self.head.bias.data.mul_(head_init_scale)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            nn.init.constant_(m.bias, 0)
+
+    def forward_features(self, x):
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+        return self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+    
+
+def convnext_tiny(input_size=32, class_num=100, **kwargs):
+    model = ConvNeXt(input_size=input_size, class_num=class_num, depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+    return model
+
+
+if __name__ == '__main__':
+    input = torch.randn(16, 3, 224, 224)
+    model = convnext_tiny(224, 100)
+    print(model)
+    y = model(input)
+    print('output shape:', y.size())
+
+    # time.sleep(10)
+    flops, params = profile(model, inputs=(input, ))
+    print('FLOPs = ' + str(flops/1000**3) + 'G')
+    print('Params = ' + str(params/1000**2) + 'M')
+
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import torchvision
 import torchvision.transforms as transforms
+import torch.backends.cudnn as cudnn
 import math
 
 sys.path.append('./')  # to run '$ python *.py' files in subdirectories
@@ -10,9 +11,11 @@ logger = logging.getLogger(__name__)
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import numpy as np
+import torch.utils
 
 
-from models import AlexNet, VGG
+from models import AlexNet, VGG, ConvNeXt
 
 def SetDevice():  
     if opt.device == '':
@@ -30,10 +33,13 @@ def SetDataset():
                                         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                       ]) #transforms.Compose([transforms.ToTensor()]) # 0~255 -> 0~1  //-1~1 , transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         
-        trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
+        data = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
         testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
+        
+        trainset, testset = torch.utils.data.random_split(data, [0.8, 0.2])
         trainLoader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=True)
         testLoader = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=True)
+        
         classes = ('apple ', 'aquarium_fish ', 'baby ', 'bear ', 'beaver  ', 'bed ', 'bee ', 'beetle ', 'bicycle ', 'bottle ', 'bowl ', 'boy ', 'bridge ', 'bus ', 'butterfly ', 'camel ', 'can ', 'castle ', 'caterpillar ', 'cattle ', 'chair ', 'chimpanzee ', 'clock ', 'cloud ', 'cockroach ', 'couch ', 'cra ', 'crocodile ', 'cup ', 'dinosaur ', 'dolphin ', 'elephant ', 'flatfish ', 'forest ', 'fox ', 'girl ', 'hamster ', 'house ', 'kangaroo ', 'keyboard ', 'lamp ', 'lawn_mower ', 'leopard ', 'lion ', 'lizard ', 'lobster ', 'man ', 'maple_tree ', 'motorcycle ', 'mountain ', 'mouse ', 'mushroom ', 'oak_tree ', 'orange ', 'orchid ', 'otter ', 'palm_tree ', 'pear ', 'pickup_truck ', 'pine_tree ', 'plain ', 'plate ', 'poppy ', 'porcupine ', 'possum ', 'rabbit ', 'raccoon ', 'ray ', 'road ', 'rocket ', 'rose ', 'sea ', 'seal ', 'shark ', 'shrew ', 'skunk ', 'skyscraper ', 'snail ', 'snake ', 'spider ', 'squirrel ', 'streetcar ', 'sunflower ', 'sweet_pepper ', 'table ', 'tank ', 'telephone ', 'television ', 'tiger ', 'tractor ', 'train ', 'trout ', 'tulip ', 'turtle ', 'wardrobe ', 'whale ', 'willow_tree ', 'wolf ', 'woman ', 'worm')
         print('training CIFA100')
         return trainLoader, testLoader, classes
@@ -47,6 +53,8 @@ def SetModel(device):
         return VGG.VGG16(opt.size).to(device)
     elif opt.model == 'VGG19':
         return VGG.VGG19(opt.size).to(device)
+    elif opt.model == 'ConvNeXt':
+        return ConvNeXt.convnext_tiny(opt.size).to(device)
 
 
 
@@ -56,7 +64,7 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default='')
     parser.add_argument('--data', type=str, default='cifa100')
     parser.add_argument('--model', type=str, default='AlexNet')
-    parser.add_argument('--size', type=int, default=32)
+    parser.add_argument('--size', type=int, default=224)
     parser.add_argument('--batch-size', type=int, default=8)
     
     opt = parser.parse_args()
@@ -65,24 +73,33 @@ if __name__ == '__main__':
     trainLoader, testLoader, classes = SetDataset()
     model = SetModel(device)
     print(model)
+
+    torch.manual_seed(0)
+    np.random.seed(0)
+    cudnn.benchmark = True
     
     # set loss optimizer
     loss_fnc = nn.CrossEntropyLoss()
-    lr = 0.01
+    lr = 4e-3
     epochs = 250
     momentum = 0.9
 
-    torch.manual_seed(0)
+    
     # scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.01, max_lr=0.1)
     # train
     for epoch in range(epochs):
         running_loss = 0.0
-        optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=1e-6, momentum=momentum, nesterov=True)
+        optimizer = optim.AdamW(model.parameters(), lr=lr, betas=(0.9, 0.999), weight_decay=0.05)
+        # optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=1e-6, momentum=momentum, nesterov=True)
     
         def one_cycle(y1=0.0, y2=1.0, steps=100):
-            return lambda x: ((1 - math.cos(x * math.pi / (steps + 1))) / 2) * (y2 - y1) + y1
+            if steps < 20:
+                return lambda x: 1e-6 + (lr/20)*steps
+            else:
+                return lambda x: ((1 - math.cos(x * math.pi / (steps + 1))) / 2) * (y2 - y1) + y1
         
-        lf = one_cycle(1, 0.01, epoch)
+        lf = one_cycle(lr, 1e-6, epoch)
+        
         scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
 
         for times, data in enumerate(trainLoader):
